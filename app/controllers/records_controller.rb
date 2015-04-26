2916cfc6604d197d12954bf746b3ff44c321b981
llawlor <primemod3@gmail.com>
2015-04-25 20:13:48 -0400

    add a new record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,4 +4,22 @@ class RecordsController < ApplicationController
     @records = current_user.records
   end
 
+  def new
+    @record = Record.new
+  end
+
+  def create
+    record = current_user.records.new(record_params)
+    
+    if record.save
+      redirect_to records_path
+    end
+  end
+
+  private
+  
+    def record_params
+      params.require(:record).permit(:json)
+    end
+
 end
