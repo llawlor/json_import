912dcb57d4789dd5ae1e9e32da8179291ad14909
llawlor <primemod3@gmail.com>
2015-04-26 22:30:42 -0400

    change ordering of records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -26,10 +26,10 @@ class RecordsController < ApplicationController
     
     # if no search term is present
     if search.blank? || key.blank?
-      @records = current_user.records.paginate(:page => params[:page])
+      @records = current_user.records.order("updated_at desc").paginate(:page => params[:page])
     # else perform full text search
     else
-      @records = current_user.records.where("to_tsvector(jsonb_extract_path_text(json, ?)) @@ plainto_tsquery(?)", key, search).paginate(:page => params[:page])
+      @records = current_user.records.where("to_tsvector(jsonb_extract_path_text(json, ?)) @@ plainto_tsquery(?)", key, search).order("updated_at desc").paginate(:page => params[:page])
       # save key to cookie
       session[:search_key] = key
     end
