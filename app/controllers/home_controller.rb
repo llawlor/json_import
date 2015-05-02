c4edb8b9ee5b0d8b323e0d3926bc6ef13ae4813b
llawlor <primemod3@gmail.com>
2015-05-01 22:58:19 -0400

    users that are signed in should be redirected to the records page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
   
 class HomeController < ApplicationController
 
-  def index; ; end
+  def index
+    # if user is signed in, show documents page
+    redirect_to records_path and return if current_user.present?
+  end
 
 end
