50eef14e5679563509ca046761ca0d982863a681
llawlor <primemod3@gmail.com>
2015-05-04 23:31:12 -0400

    refactor javascript for json inputs

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -2,14 +2,26 @@ module RecordsHelper
 
   # add classes to keys and values
   def json_as_html(json)
-    # opening html
+    # opening table html
     html = "<table class='table table-striped json-table'>"
     # for each key
     json.keys.each do |key|
-      # add html for each hash and key/value
-      html += "<tr><td class='json-text json-key'>#{key}</td><td class='json-text json-value'>#{json[key]}</td></tr>"
+      # add opening row and cell html
+      html += "<tr><td class='json-hash json-key'>";
+      # add html for the key text
+      html += "<span class='json-text'>#{key}</span>";
+      # add html for the key input
+      html += "<input type='text' class='json-input json-key form-control' value='#{key}'>";
+      # add html for the cell
+      html += "</td><td class='json-hash json-value'>";
+      # add html for the value text
+      html += "<span class='json-text'>#{json[key]}</span>";
+      # add html for the value textarea
+      html += "<textarea class='json-input json-value form-control'>#{json[key]}</textarea>";
+      # add closing cell and row html
+      html += "</td></tr>"
     end
-    # closing html
+    # closing table html
     html += "</table>"
     # return the html
     return html
