2a86ab4236067f9006d574f0e848e7b35c216322
llawlor <primemod3@gmail.com>
2015-05-04 22:59:51 -0400

    working on json input replacements

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,18 @@
 module RecordsHelper
+
+  # add classes to keys and values
+  def json_as_html(json)
+    # opening html
+    html = "<table class='table table-striped json-table'>"
+    # for each key
+    json.keys.each do |key|
+      # add html for each hash and key/value
+      html += "<tr><td class='json-text json-key'>#{key}</td><td class='json-text json-value'>#{json[key]}</td></tr>"
+    end
+    # closing html
+    html += "</table>"
+    # return the html
+    return html
+  end
+  
 end
