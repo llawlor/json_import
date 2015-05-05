938c33861d023b138176557a08c28338cc13bd9a
llawlor <primemod3@gmail.com>
2015-05-04 23:38:26 -0400

    javascript refactoring

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -3,17 +3,17 @@ module RecordsHelper
   # add classes to keys and values
   def json_as_html(json)
     # opening table html
-    html = "<table class='table table-striped json-table'>"
+    html = "<table class='table table-striped json-table col-sm-12 col-xs-12'>"
     # for each key
     json.keys.each do |key|
       # add opening row and cell html
-      html += "<tr><td class='json-hash json-key'>";
+      html += "<tr><td class='json-hash json-key col-sm-2 col-xs-12'>";
       # add html for the key text
       html += "<span class='json-text'>#{key}</span>";
       # add html for the key input
       html += "<input type='text' class='json-input json-key form-control' value='#{key}'>";
       # add html for the cell
-      html += "</td><td class='json-hash json-value'>";
+      html += "</td><td class='json-hash json-value col-sm-10 col-xs-12'>";
       # add html for the value text
       html += "<span class='json-text'>#{json[key]}</span>";
       # add html for the value textarea
